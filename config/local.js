9d9764b463fd654f2f395ae5bccc139fbf04ae92
Sai Kiran Maggidi <107541780+WYGIN@users.noreply.github.com>
2023-06-04 07:13:02 +0530

    Update local.js

--- config/local.js
+++ config/local.js
@@ -12,85 +12,27 @@ module.exports = {
   enableFileLogs: true,
   enableConsoleLogs: false,
 
-  githubApi: {
-    baseUrl: "https://api.github.com",
-    org: "Real-Dev-Squad",
-  },
-
   githubOauth: {
-    clientId: "<clientId>",
-    clientSecret: "<clientSecret>",
+    clientId: "2bfae6ca8d1170ab6ddd",
+    clientSecret: "db479962d72eaa2fa373d1b386a43033fdbbb4fb",
   },
 
   firestore: `{
-    "type": "service_account",
-    "project_id": "<project-name>",
-    "private_key_id": "<private-key>",
-    "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDaHHM8SL5skUp2\\nmbHnSBbJyk60O+nsq4hV23Ii3HLhe82Rndwji5DvAXv5AFKM9h2pQ3P2iZA7LQ4+\\nJ3BbTR1HgUDEuARlLz9C/CxKOtpM/2Hh5rKZkCxEUTulpJgW4XT16+qXdamaH2xz\\n/Cuec1H43HUuTZTj58yypCOh6SIZGOv4TQtMiz2DNKOktlhBr0GyN8+bsDgmEcoR\\n7DUeC5re2kcOHGqtzRoZhEPHWRcM40IjT0IffALUlqXQ/kYrWbbyPIHfo/ojxfeR\\nbLXLvWaCK+j9k28hvOb3nC0lWS+B/LerFzMF3q4vE1MjwAj2uU2DdhZXvTe8bnYR\\ntshsmRURAgMBAAECggEACZiWl9SF/0IRrf/jWD7/VKu4VHzt1O4zkn50CjVbfxDq\\nuZFB0BUKAH/217zURPQnmCy574L8I+Res+yZiITPKNgWlMkZVWfTn1N3seDevaQ/\\nRth262Nw1SUjTA1+rQQImLDDWpxQRNIE3CIAO8mMVdgCNlIuvquyQXpQAIXArKyr\\njsP8HFKYBb515Hn4NSO9eG8u1Mp+3hTMmwjb6k33ezDh3kJo9u0FTPOSD67GuMJv\\nXHbB96+xYDBa9vCqEHCukqtmTz1Bg3Oe47X/QlJWNOxqSBZ+ikfxWSF7DPt1r+5w\\nWHHlFsUvirHEug8tsLVmKv0hkU1TicE0ktjF8X6HGQKBgQD8/z4K1AkPX1gq/9jc\\nQcErZXBI8eYZOUJywJ5Cw4fiVq1VByqgBqr9sfJdkWi8WrWkDLCEf4h4Fy/yKClh\\nW/A5qHbkC8B9LWPiYJw++8FUpUHDPkxA+Szeq5UDkJH2aumcFwGdMywUvh0sVqwv\\nbvhYDtlanQSEGAvCd+NiQgNDyQKBgQDcszQO7S0EGpABGc6bkGvAlbmHqhUV5TzA\\ndvanSFXQ3f+MhboMUuZiDwpplvvqeAKc3Y3d2Ps6OPVD+ailL8QjN9FQ1sHZeuHy\\nckUYxaenspNdP6AWQxEAzJqHdpzV3EyJvMsDmhhaNwNSJ4vuZ53mVrh+6u4TWyks\\nUko2O2mbCQKBgQD1qov+S8K4cKbWqjVUO21tzERqMKp0l8tUToHe5qtON0h8pkbX\\nuWHUkzR7czU2oQZ8U+4b2xMTOcDO7fywk2wDMPixnE+/vZGeQp218xTaMtZW1mmJ\\nNexCFG7QVVPG6i4J6bUhho0pXypI4ai1LpZsO48HlCzMb+ULYwsjYGJ3MQKBgFZt\\ns2hZB3UA9f4IXjnbr+bme5aeS82cTVNOAz/1eu3l0kr0n6xt1pz2KOy63QKwZs2J\\nkiIb9B6T6bDqF1pBP31PQaB3ychicBOjHl4aIZLxwvYUkZvGPeVjOuzrzXWO5UZX\\nceWCNiE2RA2rQQhm+ZYXxf6mAAAChjg+LaPZVn0JAoGAXBGDDjWORrbD7T7qSTim\\n8sVCMmBX385JhWZGwE1BbdS/eOpnKR4uVRKTsDS8Q0uN3sydaHv1uTQhk2eLKaWZ\\nSzda8nPVduaIiXm79YDpALDHFdjIGcTb/s0MRNLLt6sBNw0Ytma9KHg6tzpPpJwP\\n4TwE9j91+jzusl9988Eke6s=\\n-----END PRIVATE KEY-----\\n",
-    "client_email": "firebase-adminsdk-hqc2v@dev-rds.iam.gserviceaccount.com",
-    "client_id": "<client-id>",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-hqc2v%40dev-rds.iam.gserviceaccount.com"
-  }`,
-
-  services: {
-    rdsApi: {
-      baseUrl: "https://api.realdevsquad.com",
-    },
-
-    rdsUi: {
-      baseUrl: "https://realdevsquad.com",
-      routes: {
-        authRedirection: "/goto",
-      },
-    },
-  },
+  "type": "service_account",
+  "project_id": "rds-backend-node",
+  "private_key_id": "28af700130d8b92ba6d74d300be6d5828d8f89de",
+  "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCfKdZ6slHMLF5U\\nq5CY2Bg1S1ndrs5kdSa+/MMoQeWf6BIA/rdGxeYrS3XVapAgJJbZ8XC3q+1DeH8Y\\n5bZzGaCOebtfYuAo6lGUz6hlNacksGaKXfgO9VOl/azOgzotfwT/5DP9ITTBA8X4\\nn26x2TwhKBCryZY3cJlrpeUFo/7FlpTjyAPQDEnJ1ke9qjTzoI1XKT9XKp5jJvUB\\nekg65/epLrZm3ISHGHa9j3bosRzrNN6HtlKqSc+agxgACJHpvoEZRns37PE1tAgl\\nj9TV7b3EQDp6nb5PKa6TX9jNly32WgV+4AYVT8xI7dXzpQTSNl/hpSP+Qc8KVA7o\\njVM/g07XAgMBAAECggEABGpKstijgEBy1XJ07miAM2pFseqZexvs2Tt1xnYdgpQT\ne8eVGfd/bRAI0sTD/sBFFwKll9LDIC4FiJFGwGajl5A07G53vWrSnIguKbCVVGU4\\nUvpMWU3g5iflVU06nex6HQcdkhYLupb6NO1nSrO2AU11OO68pgkPmHbfpYppFN4P\\nS/7xSkF7D/9H4MGAsrk66IrVoZNvYxlLgYBe1kARu9NYIkZO7rW1AuI/8Y/+cZKh\\nEw1ba+YKtbjcOrHueK6EXixKnJJ3ANTYioJenT62HurTuDTr62Uy29VvMn4e+Msm\\n+jUfBmx5CigZOtQo3MtxAwg1T8c6dbm+h8uPzkdBEQKBgQDM1vPbcAf3WmSV/P8G\\nTY8PN7Kvtll+nqNgxMZRFtlJO3aTpiMXNatbMpIL48oOGp/zZ+w0o2K0QxiPDyGd\\n/sqrQanXMCorD2HRrpda2yEgEzTVopUjX0XnMc7C6+U3XnxetTR+AuQyq3wgW6Yy\\nTW2n07obZtckFT0QkbWukirUKwKBgQDG6m+3jhhp4kzRkkmIvOPrtVNVRfuDq6J5\\nFrNav/OltoJZuVb4BHOf0zDW6L+hCnIv4uWmcJbB0SPaZOkKpLAYxcbGRHDhSPuP\\nRLZq03dfOO/8fvLm7psVPyq1kzeHyNoFyV/hsZxltPDPX3lfwvsoW3nFSMQxKOFy\\nt22GkMB+BQKBgBq8a7PWmZOVfs986CSAdw27bV015dR7DEQTKvdmjhHa6w02gBoF\\nbUpH/XwDzVmt6/avN8tfEAELSOdTSEYA/Z8AwRRPOxHHvjLKIQNA2r//AcRopiKp\\nysv0WlJiy9ZAROdvq1xPME3UyCzJCZZDOO7Q2de9wpL5GrFQ82/HsENlAoGAKxDk\\nwRvtozcQpKsEaqtiuUyXzzWIn9hQI+FLUZGPgchC7oQXFTl5gxfpUg4wNwCY+k8o\\no2Fq8mmCVq1UWy6Oxb5EK/bzbYMsm7cnu3cvOBmOs98UXfU49F+psog+FJLiyS/x\\nJ18Z2ldTb8Je9+GyGOm9Mn5vo6kTLpASEXlTg7UCgYAfuRg2a3WZtuBN/jOb1My6\\nfmRa0gQR5WBHuNjvw+lDBl1WCHD1+4dRTEfF9zQ3EY9qi73CxnfRkJmjt2UgO4dr\\nNBYAbr4idqVVLzfSaanGeb1jTbKVVsvrevYjrfpNlK7pwuvQ+/R3teeD+cUzkwxB\\nLxyqG3mMcwk0mNsMjQjgvg==\\n-----END PRIVATE KEY-----\\n",
+  "client_email": "firebase-adminsdk-88cdr@rds-backend-node.iam.gserviceaccount.com",
+  "client_id": "101447402106293807654",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-88cdr%40rds-backend-node.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}`,
 
   cors: {
     allowedOrigins: /(.*)/, // Allow realdevsquad.com, *.realdevsquad.com //allow all sources of traffic
   },
 
-  userToken: {
-    cookieName: `rds-session-${NODE_ENV}`,
-    ttl: 30 * 24 * 60 * 60, // in seconds
-    refreshTtl: 180 * 24 * 60 * 60, // in seconds
-    publicKey: "<publicKey>",
-    privateKey: "<privateKey>",
-  },
-
-  botToken: {
-    botPublicKey: "<botpublicKey>",
-  },
-
-  // Cloudinary keys
-  cloudinary: {
-    cloud_name: "Cloud_name",
-    api_key: "API_KEY",
-    api_secret: "api_secret_key",
-  },
-
-  // Cloudflare
-  cloudflare: {
-    CLOUDFLARE_ZONE_ID: "Cloudflare_Zone_ID_or_ID",
-    CLOUDFLARE_X_AUTH_KEY: "Cloudflare_API_Auth_Key",
-    CLOUDFLARE_X_AUTH_EMAIL: "Cloudflare_User_Email",
-  },
-
-  rdsServerlessBot: {
-    rdsServerLessPrivateKey: "RDS_SERVERLESS_PRIVATE_KEY",
-    ttl: 60,
-  },
-
-  integrations: {
-    newrelic: {
-      appName: "RDS_API_production",
-      licenseKey: "<newrelicLicenseKey>",
-    },
-  },
-
-  routesCacheTTL: {
-    "/members": 900,
-  },
 };
